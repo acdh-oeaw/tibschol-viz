91a04577c063a372cfb911b8aad93b48f7ec3da3
gythaogg <gythaogg@users.noreply.github.com>
2026-02-25 11:07:59 +0000

    deploy: 8d6d79c790e326dda2616474a356a1ad1b4e81ac

---
+++ _next/static/fH-oPpdFqZS7WMUVgoJGV/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/tibschol-viz//_next/:path+",
+        "destination": "/tibschol-viz/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
